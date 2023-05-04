f35eb60ce53fafd30b3a28d1878283e08fbd2e03
Sarke01 <mihajlosaric22@gmail.com>
2023-05-04 12:40:45 +0200

    cenovnik dark theme and animation

--- cenovnik.js
+++ cenovnik.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', () => {
     const sessionId = getCookie('session');
+    const darkMode=getCookie("darkMode");
+    if(darkMode=="true"){
+      applyDark();
+    }
     if (!sessionId) {
       // Redirect the user to the dashboard page
       window.location.href = '/login.html';
@@ -26,15 +30,60 @@ const slika2=document.getElementById('cenovnik-picture1');
 const table1=document.getElementById('table-1');
 const dugme=document.getElementById('prices');
 const dugme1=document.getElementById('prices1');
+const body=document.querySelector("body");
+const darkTheme=document.querySelector(".navigation button");
+const themeImg=darkTheme.querySelector("img");
+const logoContainer=document.querySelector(".container");
+
+
 
 dugme.addEventListener('click',function(){
-    slika.style.opacity=0;
-    dugme.style.opacity=0;
+    // slika.style.opacity=0;
+    // slika.style.width="10%";
+    dugme.style.display="none";
+    slika.style.animationName = 'disapper';
+    slika.style.animationDuration = '2s';
+    slika.style.animationTimingFunction = 'ease-in-out';
+    slika.style.animationFillMode = 'forwards';
     
 })
 
 dugme1.addEventListener('click',function(){
-    slika2.style.opacity=0;
-    dugme1.style.opacity=0;
+    // slika2.style.opacity=0;
+    // slika2.style.width="10%";
+    dugme1.style.display="none";
+    slika2.style.animationName = 'disapper';
+    slika2.style.animationDuration = '2s';
+    slika2.style.animationTimingFunction = 'ease-in-out';
+    slika2.style.animationFillMode = 'forwards';
     
+})
+
+const applyDark=()=>{
+  themeImg.src = 'images/dark-theme.png';
+  body.style.backgroundColor="#777777"
+  body.style.color="white"
+  logoContainer.style.backgroundColor="#777777"
+  logoContainer.style.color="white"
+  logoContainer.style.borderBottom="2px solid  rgb(28, 77, 135)"
+}
+
+const applyLight=()=>{
+  themeImg.src = 'images/light-theme.png';
+  body.style.backgroundColor="white"
+  body.style.color="black";
+  logoContainer.style.backgroundColor="white"
+  logoContainer.style.color="#777777"
+  logoContainer.style.borderBottom="0"
+}
+
+darkTheme.addEventListener("click",()=>{
+  if (themeImg.src.includes('dark-theme')){
+    document.cookie = "darkMode=false; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    applyLight();
+  }
+  else {
+    document.cookie = "darkMode=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+    applyDark();
+  }
 })
